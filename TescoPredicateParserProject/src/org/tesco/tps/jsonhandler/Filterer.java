59e8738734c81a3bdbe997ac961e6456132f615e
steverosstalbot <steve@pi4tech.com>
2016-03-31 12:49:40 +0100

    Added files via upload
    A bit of refactoring in preparation for others to use it in a micro service.
    Evaluation still to finish.
    Binding across filters to be investigated.

---
+++ TescoPredicateParserProject/src/org/tesco/tps/jsonhandler/Filterer.java
@@ -0,0 +1,14 @@
+package org.tesco.tps.jsonhandler;
+
+public class Filterer {
+	
+	// UserAgent.json.findEvery("(director|assistant\\s*producer): { email: }"
+	// Find all Buyer role users who are responsible for Cosmetics
+	// ALLOW User.RoleName == "Buyer" && User.AuthorizationCategory == "Cosmetics"
+	//
+	// And now in JSON queries ....
+	// 
+	// json.findEvery("");
+	// 
+
+}
